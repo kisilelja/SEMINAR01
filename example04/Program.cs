823ca0d9cab212df7d4b09b7ce9940283cf350fc
Olga <o.o.kiseleva@yandex.ru>
2022-05-13 13:19:41 +0300

    пример для вывода чисел от -n до n

---
+++ example04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int min = -N;
+while (min <= N)
+{
+    Console.Write(min);
+    Console.Write(", ");
+    min++ ;
+}
+
