1d68102721ce9e2f243973091e663080eae5dba6
Olga <o.o.kiseleva@yandex.ru>
2022-05-13 13:56:56 +0300

    пример для вывода последней цифры трехзначного числа

---
+++ example05/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int A = int.Parse(Console.ReadLine());
+int result = 0;
+if (100 <= A && A <= 999) 
+{
+    result = A % 10;
+    Console.Write ("Последняя цифра числа = ");
+    Console.WriteLine (result);
+}    
+else
+{
+    Console.Write ("Не является 3х значным числом!");
+}
