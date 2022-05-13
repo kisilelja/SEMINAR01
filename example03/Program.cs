dbf773f9cad7d4f1706d6e57e6ec9f2c17445a65
Olga <o.o.kiseleva@yandex.ru>
2022-05-13 12:51:15 +0300

    пример для максимального из 3х чисел

---
+++ example03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число № 1: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число № 2: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число № 2: ");
+int C = int.Parse(Console.ReadLine());
+int max = A;
+if (B > A) max = B;
+if (C > A) max = C;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
+
