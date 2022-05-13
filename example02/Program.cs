d1ffdc88a763600cd59d34c5889c50507317b0d5
Olga <o.o.kiseleva@yandex.ru>
2022-05-13 12:19:58 +0300

    добавили программу мин макс

---
+++ example02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число № 1: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число № 2: ");
+int numberB = int.Parse(Console.ReadLine());
+int max = numberA;
+int min = numberB;
+if (numberB > numberA)
+{
+    max = numberB;
+    min = numberA;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
+Console.Write("Минимальное число: ");
+Console.WriteLine(min);
